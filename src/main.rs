c9e8d8aa8591d76fc854f43c2fd225a05edf7109
sagudev <samo.golez@outlook.com>
2021-06-18 16:12:11 +0200

    impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use std::process::Command;
+use std::env;
+
+
+fn main() -> Result<(), Error> {
+    let status = if cfg!(unix) {
+        Command::new("./mach").args(env::args()).status()?
+    } else {
+        Command::new("mach.bat").args(env::args()).status()?
+    };
+    assert!(status.success());
+    Ok(())
 }
